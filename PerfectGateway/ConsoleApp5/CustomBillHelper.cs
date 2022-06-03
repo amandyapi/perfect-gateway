745e52cc94146c7f638b685d739a4efc435159bc
Amand <yapiamand@gmail.com>
2022-06-03 10:59:36 +0000

    Create CustomBillHelper.cs

---
+++ PerfectGateway/ConsoleApp5/CustomBillHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PerfectGateway.ConsoleApp5
+{
+    internal class CustomBillHelper
+    {
+    }
+}
